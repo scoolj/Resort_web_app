653f2eb9b99f845259ce291b52a017ef80ca659c
Scoolj <scoolj99@gmail.com>
2020-09-02 14:07:37 +0100

    filter by type, capacity donegit  add .

--- client/src/context.js
+++ client/src/context.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import items from "./data";
+import Room from "./components/Room";
 const RoomContext = React.createContext();
 //<RoomContext.Provider value={'hello'}
 class RoomProvider extends Component {
@@ -8,6 +9,15 @@ class RoomProvider extends Component {
     sortedRooms: [],
     featuredRooms: [],
     loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize:0,
+    maxSize:0,
+    breakfast: false,
+    pets: false
   };
 
   // getData
@@ -41,11 +51,52 @@ class RoomProvider extends Component {
       return room
   }
 
+  handleChange = event =>{
+       const target = event.target
+       const value = event.type === 'checkbox ' ?
+       target.checked : target.value
+       const name = event.target.name
+       this.setState(
+           {
+               [name]:value
+           }, this. filterRooms
+       )
+  }
+
+  filterRooms =() =>{
+       let {
+           rooms, type, capacity, price, minSize, maxSize, breakfast, pets
+       } = this.state
+       // all the rooms
+       let tempRooms = [...rooms]
+
+       // transform value
+       capacity = parseInt(capacity)
+       price = parseInt(price)
+
+       // filter by type
+       if(type !== 'all'){
+           tempRooms = tempRooms.filter( room => room.type ===type)
+       }
+       
+
+       // filter by  capacity
+       if(capacity !==1){
+           tempRooms = tempRooms.filter(room =>room.capacity >= capacity)
+       }
 
 
+       // filter by price
+       tempRooms = tempRooms.filter(room =>  room.price <=price)
+
+       // change state
+       this.setState({
+           sortedRooms: tempRooms
+       })
+  }
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom:this.getRoom }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom:this.getRoom, handleChange: this.handleChange }}>
         {this.props.children}
       </RoomContext.Provider>
     );
@@ -53,4 +104,13 @@ class RoomProvider extends Component {
 }
 
 const RoomConsumer = RoomContext.Consumer;
+
+export function withRoomConsumer(Component){
+    return function ConsumerWrapper(props){
+        return <RoomConsumer>
+            {value => <Component {...props} context={value} />}
+        </RoomConsumer>
+    }
+}
+
 export { RoomProvider, RoomConsumer, RoomContext };
--- client/src/pages/SingleRoom.js
+++ client/src/pages/SingleRoom.js
@@ -4,6 +4,7 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../context";
+import StyleHero from '../components/StyledHero'
 
 class SingleRoom extends Component {
   constructor(props) {
@@ -34,14 +35,60 @@ class SingleRoom extends Component {
         )
     }
 
-    const {name, description, capacity, size, price , extra, breakfast, pets, images} = room
-  return <Hero hero='roomHero'>
+    const {name, description, capacity, size, price , extras, breakfast, pets, images} = room
+
+    const [mainImg, ...defaultImg] = images
+  return (
+      <>
+        <StyleHero img={mainImg || this.state.defaultBcg}>
       <Banner title={`${name} room`}>
           <Link to='/rooms' className="btn-primary">
               back to rooms
           </Link>
       </Banner>
-  </Hero>
+  </StyleHero>
+    <section className="single-room">
+        <div className="single-room-images">
+            {defaultImg.map((item, index) =>{
+                return <img key={index} src={item} alt={name} />
+            })}
+        </div>
+
+        <div className="single-room-info">
+            <article className="desc">
+                <h3>details</h3>
+        <p>{description}</p>
+            </article>
+
+            <article className="info">
+                <h3>info</h3>
+        <h6>price : ${price}</h6>
+        <h6>size:  ${size} SQFT </h6>
+        <h6>
+            max capacity : {
+                capacity > 1 ? `${capacity} people` : `${capacity} person`
+            }
+        </h6>
+        <h6>
+            {pets ? "pets allowed" : "no pet allowed"}
+        </h6>
+        <h6>{breakfast && "free breakfast included"}</h6>
+            </article>
+        </div>
+    </section>
+    <section className="room-extras">
+        <h6>
+            extras
+        </h6>
+        <ul className="extras">
+            {extras.map((item, index) => {
+                return <li key={index}>- {item}</li>
+            })}
+        </ul>
+    </section>
+
+  </>
+  ) 
   }
 }
 
